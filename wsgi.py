6c88e0008cef7d49cf09d79d0ba9e251e500f4b4
lucascapelli <alcantaraminacapellilucas@gmail.com>
2026-01-27 14:41:40 -0300

    fix variavel app wsgi

--- wsgi.py
+++ wsgi.py
@@ -1,17 +1,6 @@
-from app import create_app, get_services
-from app.background import start_background_tasks
-import logging
-
-# Configuração de Logs
-logging.basicConfig(level=logging.INFO)
-logger = logging.getLogger("wsgi")
+from app import create_app
 
 app = create_app()
-services = get_services(app)
-
-# Iniciar tarefas em background
-# Nota: Usar gunicorn com -w 1 para evitar duplicação de tarefas de background
-start_background_tasks(services)
 
 if __name__ == "__main__":
     app.run()
